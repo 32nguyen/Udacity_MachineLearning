1b4c421949cc30e2e425f882fa06f1773e9f78e5
32nguyen <32nguyen@cardinalmail.cua.edu>
2016-04-11 23:06:28 -0400

    add some quiz on Decision tree

--- ud120-projects-master/svm/svm_author_id.py
+++ ud120-projects-master/svm/svm_author_id.py
@@ -9,9 +9,13 @@
 """
     
 import sys
+sys.path.append("../../Lecture 1/")
+sys.path.append("../tools")
+
 from time import time
-sys.path.append("../tools/")
+
 from email_preprocess import preprocess
+from sklearn.svm import SVC
 
 
 ### features_train and features_test are the features for the training
@@ -19,11 +23,21 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
 #########################################################
-### your code goes here ###
+
+clf = SVC(kernel="rbf", C=10000)
+clf.fit(features_train, labels_train)
+pre = clf.predict(features_test)
+
+count = 0.0
+for i in range(0, len(pre)):
+    if pre[i] == 1:
+        count += 1
+print count
+accuracy = clf.score(features_test, labels_test)
+print 'the accruracy is :', accuracy
 
 #########################################################
 
